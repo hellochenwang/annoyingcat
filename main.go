fc0d5804ac276f0a6c7a44f13bade8935a9f3673
Chen W <hello.chen.wang@gmail.com>
2020-06-23 23:17:47 -0700

    2. save the data to mysql database

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func dbConn() (db *sql.DB) {
+	dbDriver := "mysql"
+	dbUser := "root"
+	dbPass := "password"
+	dbName := "test"
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+	//	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/test")
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
@@ -16,6 +32,7 @@ func main() {
 
 type orderStruct struct {
 	ProductID        string `json:"ProductID"`
+	Email            string `json:"Email"`
 	Address          string `json:"Address"`
 	Zip              string `json:"Zip"`
 	CreditCardNumber string `json:"CreditCardNumber"`
@@ -37,4 +54,13 @@ func orderHandler(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 	log.Println("JSON ", order)
+
+	db := dbConn()
+	// perform a db.Query insert
+	insert, err := db.Query("INSERT INTO test.order VALUES ( ?,?,? )", order.ProductID, order.Email, order.CreditCardNumber)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer insert.Close()
+	defer db.Close()
 }
